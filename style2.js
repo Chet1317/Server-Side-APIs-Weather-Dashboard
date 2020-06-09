1f467b7f1c36bd91cba95848099dbc6c66776e2c
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-06-08 22:56:59 -0400

    added new js script, creating new variables and html as well as style for uvi

---
+++ style2.js
@@ -0,0 +1,106 @@
+
+
+
+function submit(){
+    event.preventDefault() 
+    var key = "94a8ffec8561a3c29ea48d78f0dd77b6";
+    var allCity = document.querySelector("#inputBox").value
+    submitCity(key,allCity)
+   
+}
+
+async function submitCity(key,allCity){
+    var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${allCity}&appid=${key}`;
+    const fetchData = await fetch(weatherUrl)
+    console.log(fetchData)
+    const fetchJsonData = fetchData.json()
+    fetchJsonData.then(function(data){
+        console.log(data)
+        var dataLat = data.coord.lat
+        var dataLon = data.coord.lon
+        console.log(`${dataLon} ${dataLat}`)
+    submitWeather(key, dataLat, dataLon)
+    currentWeather(key, dataLat, dataLon)
+    
+    
+    })
+} 
+// Five day forecast
+async function submitWeather (key, dataLat, dataLon){
+    var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${dataLat}&lon=${dataLon}&units=metric&appid=${key}`;
+    const fetchData = await fetch(forecastUrl)
+    console.log(fetchData)
+    var fetchJsonData = fetchData.json()
+    fetchJsonData.then(function(data){
+    
+        console.log(data)
+        console.log(data.list)
+        console.log(data.list[0])
+        
+        showWeather(data)
+      
+    })
+}
+
+ //show Forecast objects
+ function showWeather(data){
+    for (var i=1; i<=5; i++)
+    console.log(data.list[i])
+    var htmlstring = "";
+    var htmlstring1 = "";
+    var htmlstring2 = "";
+    var htmlstring3 = "";
+    var htmlstring4 = "";
+    var openPara = document.getElementById("openPara")
+    var openPara2 = document.getElementById("openPara2")
+    var openPara3 = document.getElementById("openPara3")
+    var openPara4 = document.getElementById("openPara4")
+    var openPara5 = document.getElementById("openPara5")
+    
+    openPara.innerHTML+=htmlstring+=data.list[0].main.temp
+    
+    openPara2.innerHTML+=htmlstring1+=data.list[1].main.temp
+   
+    openPara3.innerHTML+=htmlstring2+=data.list[2].main.temp
+    
+    openPara4.innerHTML+=htmlstring3+=data.list[3].main.temp
+   
+    openPara5.innerHTML+=htmlstring4+=data.list[4].main.temp
+
+
+    }
+
+//Current weather
+async function currentWeather (key, dataLat, dataLon){
+    var currentUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${dataLat}&lon=${dataLon}&units=metric&appid=${key}`;
+    const fetchData = await fetch(currentUrl)
+    console.log(fetchData)
+    var fetchJsonData = fetchData.json()
+    fetchJsonData.then(function(data){
+        var dataNme = data.current.uvi
+        console.log(data)
+        console.log(data.list)
+        showCurrent(dataNme)
+        
+    })}
+
+    
+     function showCurrent (dataNme){
+        
+        var htmlstring1 = "";
+        var openPara1 = document.getElementById("openPara1")
+    openPara1.innerHTML+=htmlstring1+=dataNme
+    console.log(dataNme)
+    
+    if(dataNme > 5){
+        console.log("this is hot")
+        document.getElementById("openPara1").style.color = "red";
+
+    }else if(dataNme < 5){
+        document.getElementById("openPara1").style.color = "blue";
+    }
+        console.log("this is cold");
+    }
+    
+
+   
